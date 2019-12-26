ff89d484bbf8d06b68cf2bdeca9b79f169efe7d4
denisia97 <artenidenisia@yahoo.com>
2019-12-26 16:13:59 +0200

    read data training

---
+++ OcupancyDetection/OcupancyDetection/DataSet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OcupancyDetection
+{
+    public class DataSet
+    {
+        public int size;
+       public InputData[] instanta { get; set; } //lista cu instantele de antrenare
+       public double [] y { get; set; } //iesirea corespunzatoare instanta[i] are iesirea y[i]
+
+        public DataSet(int size)
+        {
+            this.size = size;
+            instanta = new InputData[size];
+            y = new double[size];
+        }
+    }
+}
--- OcupancyDetection/OcupancyDetection/Form1.cs
+++ OcupancyDetection/OcupancyDetection/Form1.cs
@@ -15,5 +15,31 @@ namespace OcupancyDetection
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        
+            string[] lines = System.IO.File.ReadAllLines("Dataset/datatraining.txt");
+
+            DataSet dataset = new DataSet(lines.Length - 1);
+            String[] args;
+            InputData inputData;
+            int nrAtributes;
+
+            for (int i= 1;i< lines.Length ;i++) //prima linie este capul de tabel
+            {
+               args = lines[i].Replace("\"", "").Split(','); //campurile instantei
+               nrAtributes = args.Length - 2; //primul e un id ce poate fi ignorat, iar ultimul este clasa
+               inputData = new InputData(nrAtributes);
+               inputData.x[0] = DateTime.Parse(args[1]).ToOADate();
+               for(int j=1;j<nrAtributes;j++)
+                {
+                    inputData.x[j] = Double.Parse(args[j + 1]);
+                }
+                dataset.instanta[i - 1] = inputData;
+                dataset.y[i - 1] = Double.Parse(args[args.Length - 1]);
+
+            }
+        }
     }
 }
